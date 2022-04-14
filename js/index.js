636f9fa9dd8e9e5f9177d6fbf8d8056f61453c24
Mango <45252427+2934301923@users.noreply.github.com>
2022-04-14 17:32:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var logo_app = "img/logo.png";
 	var fabu_app = "发布时间：2022年2月13日丨 Xiaoyu technical team";
 	var dibu_app = "Copyright Xiaoyu technical team © All Rights Reserved";
 	var ewm_app = "img/ewm.jpg";
-	var xiaz_app = "https://hyl-bilibili.lanzouq.com/iQkDu00gn4nc";
+	var xiaz_app = "https://hyl-bilibili.lanzouq.com/iwH6R036oove";
 	var text_app = "注册码计算器重置版是基于Apktool+助手的界面注册码计算器重置而来，支持安卓13，利用新特性开发，比原版更快更稳定！";
 
 	var win_width = $(window).width();
