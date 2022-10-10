49ea6f3ad7d6b472cbf44693c4aeafed5a396f37
Mango <45252427+2934301923@users.noreply.github.com>
2022-10-10 20:52:29 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var logo_app = "img/logo.png";
 var title_app = "注册码计算器";
 var shouji_app = "https://img.gejiba.com/images/6ea7f61b82b8c17310703ed96501cf7f.png";
-var banb_app = "版本：1.3.6 丨大小：6.1M|";
-var fabu_app = "发布时间：2022年09月11日 丨 Xiaoyu technical team";
+var banb_app = "版本：1.3.7 丨大小：6.21M|";
+var fabu_app = "发布时间：2022年10月10日 丨 Xiaoyu technical team";
 var dibu_app = "Copyright Xiaoyu technical team © All Rights Reserved";
 var ewm_app = "img/ewm.jpg";
-var xiaz_app = "https://hyl-bilibili.lanzoux.com/inThx0bf1q3c";
+var xiaz_app = "https://hyl-bilibili.lanzoux.com/iVqFj0dkmrng";
 var text_app = "注册码计算器重置版是基于Apktool+助手的界面注册码计算器重置而来，支持安卓13，利用新特性开发，比原版更快更稳定！";
 
 var win_width = $(window).width();
